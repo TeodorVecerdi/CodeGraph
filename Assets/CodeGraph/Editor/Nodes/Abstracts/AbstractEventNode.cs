89579f01116b1b48f2bc0831189a1a5085597a44
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-27 14:10:53 +0200

    Add delete button to event node ports

--- Assets/CodeGraph/Editor/Nodes/Abstracts/AbstractEventNode.cs
+++ Assets/CodeGraph/Editor/Nodes/Abstracts/AbstractEventNode.cs
@@ -1,35 +1,63 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace CodeGraph.Editor {
     public abstract class AbstractEventNode : AbstractNode {
         public int PortCount;
         public bool IsBaseEventNode;
+
         protected AbstractEventNode() {
             // capabilities &= ~Capabilities.Deletable;
             IsBaseEventNode = true;
         }
+
         [Obsolete("Event nodes cannot have Input ports", true)]
         public new void AddInputPort(Port portReference, Func<string> requestCode) {
             throw new NotSupportedException("Event nodes cannot have Input ports");
         }
-        
+
         public void AddChildPort(bool incrementPortCount = true) {
             var outputPort = base.InstantiatePort(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(float));
             outputPort.portName = $"child {OutputPorts.Count + 1}";
-            outputPort.portColor = new Color(1,1,1,0.2f);
-            AddOutputPort(outputPort, () => "");
+            outputPort.portColor = new Color(1, 1, 1, 0.2f);
+
+            // AddOutputPort(outputPort, () => "");
+            var outputPortObj = new OutputPort(this, outputPort, () => "");
+            OutputPorts.Add(outputPortObj);
+            OutputPortDictionary.Add(outputPort, outputPortObj);
+            outputPort.contentContainer.Add(new Button(() => { RemovePort(outputPortObj);}){text = "X"});
+            outputContainer.Add(outputPort);
+
             if (incrementPortCount) PortCount++;
             Refresh();
         }
+
+        private void RemovePort(OutputPort port) {
+            var connectionsToRemove = new List<Edge>();
+            connectionsToRemove.AddRange(port.PortReference.connections);
+            while(connectionsToRemove.Count > 0) {
+                var conn = connectionsToRemove[0];
+                CodeGraph.Instance.GraphView.RemoveElement(conn);
+                conn.input.Disconnect(conn);
+                conn.output.Disconnect(conn);
+                // conn = null;
+                connectionsToRemove.RemoveAt(0);
+            }
+            outputContainer.Remove(port.PortReference);
+            port.PortReference = null;
+            OutputPorts.Remove(port);
+        }
+
         public void CleanPorts() {
             var portsToRemove = new List<OutputPort>();
-            (from port in OutputPorts 
-                    let portConnections = port.PortReference.connections.ToList() 
-                    where portConnections.Count == 0 
+            (from port in OutputPorts
+                    let portConnections = port.PortReference.connections.ToList()
+                    where portConnections.Count == 0
                     select port).ToList()
                 .ForEach(port => {
                     outputContainer.Remove(port.PortReference);
