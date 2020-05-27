27e37bbbbf09960eebb4f3cb633c262745a8c73f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-27 13:01:45 +0200

    Add support for codegraph files schema version

--- Assets/CodeGraph/Runtime/CodeGraphData.cs
+++ Assets/CodeGraph/Runtime/CodeGraphData.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 namespace CodeGraph {
     [Serializable] public class CodeGraphData {
+        public static uint SchemaVersionLatest = 1;
+        
         [SerializeField] public string AssetPath;
         [SerializeField] public string GraphName;
         [SerializeField] public string LastEditedAt = "0";
+        [SerializeField] public uint SchemaVersion = SchemaVersionLatest;
         [SerializeField] public bool IsMonoBehaviour;
         [SerializeField] public List<SerializedNode> Nodes = new List<SerializedNode>();
         [SerializeField] public List<SerializedEdge> Edges = new List<SerializedEdge>();
--- Assets/CodeGraph/Runtime/SerializationHelper.cs
+++ Assets/CodeGraph/Runtime/SerializationHelper.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using CodeGraph.Editor;
-using GitHub.ICSharpCode.SharpZipLib.Zip;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
