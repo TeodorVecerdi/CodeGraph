76ea2db8c8c884a2111d265506ad0b4cc128f328
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-31 20:26:22 +0200

    Remove reference to Github.Unity package

--- CodeGraph_Unity/Assets/CodeGraph/Editor/Nodes/EndNodes/CallMethodNode.cs
+++ CodeGraph_Unity/Assets/CodeGraph/Editor/Nodes/EndNodes/CallMethodNode.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using GitHub.Unity;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEditor.Experimental.GraphView;
--- CodeGraph_Unity/Assets/CodeGraph/Editor/Nodes/StartNodes/CreateMethodNode.cs
+++ CodeGraph_Unity/Assets/CodeGraph/Editor/Nodes/StartNodes/CreateMethodNode.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using GitHub.Unity;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEditor.Experimental.GraphView;
