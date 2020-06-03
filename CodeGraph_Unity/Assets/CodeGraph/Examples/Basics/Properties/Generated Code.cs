2fcccb27249a08b8b6636efc89950b998411d97b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-06-03 13:41:23 +0200

    Add a few examples

---
+++ CodeGraph_Unity/Assets/CodeGraph/Examples/Basics/Properties/Generated Code.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class Properties : MonoBehaviour {
+public float Speed;//BEGIN_NODE_GUID/2fec54f5-6ce3-429d-96b5-796a43eea6b7/END_NODE_GUID
+private void Start() {
+Debug.Log("Your speed is: "+Speed);//BEGIN_NODE_GUID/c538b256-3414-445d-974a-fa220223dd6b/END_NODE_GUID
+}
+
+}
